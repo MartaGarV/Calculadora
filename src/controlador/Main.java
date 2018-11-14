2306a1a1955aabe5c1335ba20954d09315da571e
BorjaSuMo <borja_193_8@hotmail.com>
2018-11-14 16:35:56 +0100

    Probando

---
+++ src/controlador/Main.java
@@ -0,0 +1,19 @@
+package controlador;
+
+
+import vista.Calculadora;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		Calculadora miCalculadora = new Calculadora();
+	
+	}
+
+}
+
+
+ 
+
+
